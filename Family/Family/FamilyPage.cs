b8e2de93fb00a18cc90d920f4a57de2d1f5f6283
BCP27 <gamemeister27@gmail.com>
2017-02-06 20:57:51 -0600

    Changed content label with spans to stacklayout with children labels, conent is now scroll view which calls the stacklayout

--- Family/Family/FamilyPage.cs
+++ Family/Family/FamilyPage.cs
@@ -11,46 +11,112 @@ namespace Family
     {
         public FamilyPage()
         {
-            Content = new Label
+            Padding = new Thickness(5, Device.OnPlatform(20, 5, 5), 5, 5);
+            Color textColor = new Color(255, 0, 0);
+            double fontsize = Device.GetNamedSize(NamedSize.Medium, typeof(Label));
+            double namesize = Device.GetNamedSize(NamedSize.Large, typeof(Label));
+            StackLayout stacklayout = new StackLayout
             {
-                FormattedText = new FormattedString
+
+                Children =
                 {
-                    Spans =
+                    new Label
                     {
-                        new Span
-                        {
-                            Text = "Scott Payne",
-                            FontAttributes = FontAttributes.Bold
-                            //making change quick
-                        },
-                        new Span
-                        {
-                            Text = " - Dad\n"
-                        },
-                        new Span
-                        {
-                            Text = "Mary Cosgrove",
-                            FontAttributes = FontAttributes.Bold
-                        },
-                        new Span
-                        {
-                            Text = " - Mom \n"
-                        },
-                        new Span
-                        {
-                            Text = "Carolyn Cosgrove Payne",
-                            FontAttributes = FontAttributes.Bold
-                        },
-                        new Span
-                        {
-                            Text = " - Sister"
-                        }
+                        Text = "Scott Payne\n\n",
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = namesize,
+                        TextColor = Color.Green
+                    },
+                    new Label
+                    {
+                        Text = " - Dad\n\n\n\n",
+                        TextColor = textColor,
+                        FontSize = fontsize
+                    },
+                    new Label
+                    {
+                        Text = "Mary Cosgrove\n\n",
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = namesize,
+                        TextColor = Color.Maroon
+                    },
+                    new Label
+                    {
+                        Text = " - Mom\n\n\n\n",
+                        TextColor = textColor,
+                        FontSize = fontsize
+                    },
+                    new Label
+                    {
+                        Text = "Carolyn Cosgrove Payne\n\n",
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = namesize,
+                        TextColor = Color.Pink
+                    },
+                    new Label
+                    {
+                        Text = " - Sister\n\n\n\n",
+                        TextColor = textColor,
+                        FontSize = fontsize
+                    },
+                    new Label
+                    {
+                        Text = "Alan Amundsen\n\n",
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = namesize,
+                        TextColor = Color.Purple
+                    },
+                    new Label
+                    {
+                        Text = " - Cousin\n\n\n\n",
+                        TextColor = textColor,
+                        FontSize = fontsize
+                    },
+                    new Label
+                    {
+                        Text = "Tony Amundsen\n\n",
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = namesize,
+                        TextColor = Color.Silver
+                    },
+                    new Label
+                    {
+                        Text = " - Cousin\n\n\n\n",
+                        TextColor = textColor,
+                        FontSize = fontsize
+                    },
+                    new Label
+                    {
+                        Text = "Scott Amundsen\n\n",
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = namesize,
+                        TextColor = Color.Black
+                    },
+                    new Label
+                    {
+                        Text = " - Uncle\n\n\n\n",
+                        TextColor = textColor,
+                        FontSize = fontsize
+                    },
+                    new Label
+                    {
+                        Text = "Judy Amundsen\n\n",
+                        FontAttributes = FontAttributes.Bold,
+                        FontSize = namesize,
+                        TextColor = Color.Fuchsia
+                    },
+                    new Label
+                    {
+                        Text = " - Aunt\n\n\n\n",
+                        TextColor = textColor,
+                        FontSize = fontsize
                     }
-                },
-                TextColor = new Color(255,0,0),
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.Center,
-                FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Label)),
+                }
+                
+            };
+            Content = new ScrollView
+            {
+                Content = stacklayout
             };
             BackgroundColor = new Color(0, 0, 128);
            
