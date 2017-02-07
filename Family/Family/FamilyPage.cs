ca9f60f5522d1fda6b91191d0aa238e6eca94284
htcstudent <htcstudent@EH255WD19>
2017-02-06 19:44:37 -0600

    Add project files.

---
+++ Family/Family/FamilyPage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Family
+{
+    class FamilyPage : ContentPage
+    {
+        public FamilyPage()
+        {
+            Content = new Label
+            {
+                FormattedText = new FormattedString
+                {
+                    Spans =
+                    {
+                        new Span
+                        {
+                            Text = "Scott Payne",
+                            FontAttributes = FontAttributes.Bold
+                        },
+                        new Span
+                        {
+                            Text = " - Dad\n"
+                        },
+                        new Span
+                        {
+                            Text = "Mary Cosgrove",
+                            FontAttributes = FontAttributes.Bold
+                        },
+                        new Span
+                        {
+                            Text = " - Mom \n"
+                        },
+                        new Span
+                        {
+                            Text = "Carolyn Cosgrove Payne",
+                            FontAttributes = FontAttributes.Bold
+                        },
+                        new Span
+                        {
+                            Text = " - Sister"
+                        }
+                    }
+                },
+                TextColor = new Color(255,0,0),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = Device.GetNamedSize(NamedSize.Default, typeof(Label)),
+            };
+            BackgroundColor = new Color(0, 0, 128);
+           
+            //ContentPage content = new ContentPage
+            //{
+            //    Title = "Family",
+            //    Content = new StackLayout
+            //    {
+            //        VerticalOptions = LayoutOptions.Center,
+            //        Children = {
+            //            new Label {
+            //                HorizontalTextAlignment = TextAlignment.Center,
+            //                Text = "Scott Payne "
+            //            }
+            //        }
+            //    }
+            //};
+        }
+    }
+}
